5c0b2528651a62742481a0d499331776d7384634
Tathya Dixit <dixit.tathya2003@gmail.com>
2024-07-03 20:36:35 +0530

    proxy target change

--- factontes/vite.config.js
+++ factontes/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port : 3000,
     proxy : {
       '/api': {
-        target : 'http://127.0.0.1:8000',
+        target : 'https://factnotesbackend.onrender.com',
         changeOrigin : true,
       }
   }
