eb28989053d912861a72194e13c28c3231c55276
Tathya Dixit <dixit.tathya2003@gmail.com>
2024-07-03 21:29:39 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port : 3000,
     proxy : {
       '/api': {
-        target : 'https://factnotesbackend.onrender.com',
+        target : 'https://factnotesbackend.onrender.com:8000',
         changeOrigin : true,
       }
   }
